9a464b6e94b69311f240056aa96f2d89dd0cb853
USENGROUP\h-hiranishi <h-hiranishi@usen-next.jp>
2023-06-21 10:46:49 +0900

    migrationの追加とテーブルの作成 #7

---
+++ database/migrations/2023_06_20_122613_create_credits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('credits', function (Blueprint $table) {
+            $table->id();
+            // 暗号化するので text型
+            $table->text('card_number');
+            $table->text('security_code');
+            $table->timestamp('expiration');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('credits');
+    }
+};
