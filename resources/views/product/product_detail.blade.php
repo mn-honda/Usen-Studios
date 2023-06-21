8640f1fb2b99678599a3ecf920990d2b1b649887
kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-21 17:50:53 +0900

    product_detail編集#12

--- resources/views/product/product_detail.blade.php
+++ resources/views/product/product_detail.blade.php
@@ -7,27 +7,32 @@
 </head>
 <body>
     <form action="cart/add" method="post">
-    @foreach($product_detail as $product)
-        @foreach($product->images as $image)
-            <img src="{{$images->image}}" alt="">
-        @endforeach
-        <p>{{$product->name}}</p>
-        <p>{{$product->price}}</p>
+            @foreach($product_detail->images as $image)
+                <img src="{{$images->image}}" alt="">
+            @endforeach
+            <p>{{$product_detail->name}}</p>
+            <p>{{$product_detail->price}}</p>
 
-        <p>商品詳細：<br>{{$product->name}}</p>
-        サイズ：<select name="size">
-        @foreach($product->sizes as $size)
-            <option value="{{$size->size}}">{{$size->size}}</option>
-        @endforeach
-        </select>
-        <select name="quantity">
-        @for($i = 1;i <= 10;1++)
-            <option value="{{$i}}">{{$i}}</option>
-        </select>
-        @endfor
-        <input type="submit" value="かーとにいｒ">
-        <p>{{$product->name}}</p>
-    @endforeach
+            <p>商品詳細：<br>{{$product_detail->name}}</p>
+            サイズ：<select name="size">
+            @foreach($product_detail->sizes as $size)
+                <option value="{{$size->size}}">{{$size->size}}</option>
+            @endforeach
+            </select>
+            <select name="quantity">
+            @for($i = 1;$i <= 10;$i++)
+                <option value="{{$i}}">{{$i}}</option>
+            </select>
+            @endfor
+            
+        <input type="submit" value="カートに入れる">
+        <input type="hidden" value="{{$product_detail->id}}">
+        @csrf
     </form>
+    <p>
+        ※消費税が含まれています
+        ※送料は別途発生します
+    </p>
+
 </body>
 </html>
