124b4cbb5fa9d0c8efe5c39a5509f0f8d8a0eb72
USENGROUP\h-hiranishi <h-hiranishi@usen-next.jp>
2023-06-21 11:44:01 +0900

    モデルの作成 #25

--- history_commands.sh
+++ history_commands.sh
@@ -1,3 +1,4 @@
+# マイグレーション
 sail artisan make:migration add_information_column_to_users_table --table=users
 sail artisan make:migration create_credits_table --create=credits
 sail artisan make:migration create_inquiries_table --create=inquiries
@@ -12,3 +13,17 @@ sail artisan make:migration create_carts_table --create=carts
 sail artisan make:migration create_sales_table --create=sales
 sail artisan make:migration create_deliveries_table --create=deliveries
 sail artisan make:migration create_sale_details_table --create=sale_details
+
+# モデル
+sail artisan make:model Credit
+sail artisan make:model Inquiry
+sail artisan make:model Category
+sail artisan make:model Product
+sail artisan make:model Image
+sail artisan make:model Size
+sail artisan make:model Stock
+sail artisan make:model Purchase
+sail artisan make:model Cart
+sail artisan make:model Sale
+sail artisan make:model Delivery
+sail artisan make:model SaleDetail
