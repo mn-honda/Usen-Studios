72f916b5e8470bb6a46ea0743d2052a69172d7b7
USENGROUP\mm-okuda <mm-okuda@usen-next.jp>
2023-06-21 18:17:12 +0900

    仕入れ登録、リスト作成

---
+++ resources/views/admin/purchase_register.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>仕入れ登録</title>
+</head>
+<body>
+    <h1>仕入れ登録</h1>
+    <form action="" method="post">
+        商品：
+        <select name="product">
+            @foreach ($products as $product)
+                <option value={{$product->id}}>{{$product->name}}</option>
+            @endforeach
+        </select><br>
+        個数：
+        <input type="number" name="quantity" value="" min="1"><br>
+        日付：
+        <input type="date" name="date"><br>
+        <input type="submit" value="登録">
+        @csrf
+    </form>
+    <a href="purchase_list">仕入れ一覧画面</a>
+</body>
+</html>
