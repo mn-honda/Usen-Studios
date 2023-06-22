ca42549a30528c9b669fc3ba43fb64616f8acafa
USENGROUP\mm-okuda <mm-okuda@usen-next.jp>
2023-06-22 18:35:08 +0900

    ログインページのCSS

--- resources/views/layouts/guest_base.blade.php
+++ resources/views/layouts/guest_base.blade.php
@@ -15,19 +15,16 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
-            <!--<div>
-            <a href="/">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div>
+                <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
-            </div>-->
-            <div class="text-center text-9xl font-medium">U s e n  S t u d i o s</div>
-            <div><br><br><br><br></div>
-            <div class="text-center text-xl">サインイン</div>
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white overflow-hidden sm:rounded-lg">
-                {{ $slot }}
             </div>
 
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
         </div>
     </body>
 </html>
