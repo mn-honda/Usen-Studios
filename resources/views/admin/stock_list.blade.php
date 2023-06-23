f63485e9f24958b49d4e304648ae1a59ed0d1941
USENGROUP\mm-okuda <mm-okuda@usen-next.jp>
2023-06-23 18:03:23 +0900

    画像登録時のファイルパス変更

--- resources/views/admin/stock_list.blade.php
+++ resources/views/admin/stock_list.blade.php
@@ -10,15 +10,15 @@
     <div><br><h1 class="text-3xl flex justify-center">在庫一覧</h1><br></div>
         <table class="flex justify-center">
             <tr class="bg-gray-300 border-b text-xl border border-gray-600">
-                <th class="px-10 py-5">プロダクトID</th>
-                <th class="px-10 py-5">プロダクト名</th>
+                <th class="px-10 py-5">商品ID</th>
+                <th class="px-10 py-5">商品名</th>
                 <th class="px-10 py-5">個数</th>
             </tr>
             @foreach($stocks as $stock)
                 <tr class="bg-gray-100 text-center border border-gray-600">
-                    <td>{{$stock->product_id}}</td>
-                    <td>{{$stock->product->name}}</td>
-                    <td>{{$stock->stock}}</td>
+                    <td class="py-2">{{$stock->product_id}}</td>
+                    <td class="py-2">{{$stock->product->name}}</td>
+                    <td class="py-2">{{$stock->stock}}</td>
                 </tr>
             @endforeach
         </table>
