c0cb3e81a7c72dd23200d4213ce0bb631aec624d
USENGROUP\mn-honda <mn-honda@usen-next.jp>
2023-06-23 15:10:55 +0900

    index.blade.phpあらかた完了

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +11,49 @@
     <header>
         <div class="header_container">
         <!-- メニューアイコン -->
-            <div class="menu_container">
+        <div class="menu_container">
+            <div>
                 <img class="menu_open" src ="/icon/ham-menu-open.png">
-                <img class="menu_close" src="/icon/ham-menu-close.png">
             </div>
+            <!-- <img class="menu_close" src="/icon/ham-menu-close.png"> -->
+        
+            <!-- 検索バナー -->
+            <div class="search_container">
+                <form class="searchForm">
+                    <input type="search" class="text" placeholder="検索">
+                    <button type="submit" value="検索"><img class="search_icon" src="/icon/search-icon.png" alt="" wdth="10" height="10"></button>
+                </form>
+            </div>
+        
+                        <!-- アカウントアイコン -->
+            <div class="acount_container">
+                <a href="">
+                    <img class="acountIcon" src="/icon/account_icon.png">
+                </a>
+
+                <a href="/cart">
+                    <img class="cartIcon" src="/icon/cart_icon.png">
+                </a>
+            </div>
+            <!-- カートアイコン -->
+            <!-- <div class="cart_container">
+                <a href="/cart">
+                    <img class="cartIcon" src="/icon/cart_icon.png">
+                </a>
+            </div> -->
+        </div>
+
+            <!-- <nav class="acount">
+                <ul>
+                    <li class="acountList">
+                        <a href="">メンバー登録</a><br>
+                        <a href="">サインイン</a>
+                    </li>
+
+                </ul>
+            </nav> -->
+
+
             
             <!-- メニューバナーのナビゲーション -->
             <nav class="category_menu">
@@ -37,92 +77,116 @@
                 </ul>
             </nav>
 
-            <!-- 検索アイコン -->
-            <div class="search_container">
+            <!-- 検索バナー -->
+            <!-- <div class="search_container">
                 <form class="searchForm">
                     <input type="search" class="text" placeholder="検索">
                     <button type="submit" value="検索"><img class="search_icon" src="/icon/search-icon.png" alt="" wdth="10" height="10"></button>
                 </form>
-            </div>
+            </div> -->
 
             <!-- アカウントアイコン -->
-            <div class="acount_container">
+            <!-- <div class="acount_container">
                 <a href="">
-                    <img src="/icon/account_icon.png">アカウント
+                    <img class="acountIcon" src="/icon/account_icon.png">アカウント
                 </a>
-            </div>
+            </div> -->
 
             <nav class="acount">
                 <ul>
-                    <li>
-                        <a href="">メンバー登録</a>
-                    </li>
-                    <li>
+                    <li class="acountList">
+                        <a href="">メンバー登録</a><br>
                         <a href="">サインイン</a>
                     </li>
                 </ul>
             </nav>
 
             <!-- カートアイコン -->
-            <div class="cart_container">
+            <!-- <div class="cart_container">
                 <a href="/cart">
-                    <img src="/icon/cart_icon.png">カート
+                    <img class="cartIcon" src="/icon/cart_icon.png">カート
                 </a>
-            </div>
+            </div> -->
         </div>
     </header>
 
     <!-- 画面全体 div -->
     <div class="main_container">
         <div class="logo_container">
-            <img src="/icon/UsenStudios_logo.png">
+            <img class="U-logo" src="/icon/UsenStudios_logo.png">
         </div>
         <div class="headLine">
             <h1 class="U-S">UsenStudios</h1>
         </div>
+        <div class="USexp">
+            <p class="USexp_message">
+                Usen Studios は USEN-NEXT GROUP 23卒入社の
+                <br>
+                ITエンジニア配属のH.H, K.S, M.O, M.H の4人で立ち上げた                UNG史上初のアパレルブランドです。
+                <br>
+                <br>
+                当コレクションは、デザインとディテール両方に対する
+                <br>
+                4人のこだわりを色濃く反映し、丁寧な縫製を重視しつつ様々な素材や独自開発のファブリックを使用しています。
+                <br>
+                <br>
+                当コレクションでは、men’sおよびwomen’sの
+                <br>
+                レディ・トゥ・ウェア、フットウェア、アクセサリー、デニムアイテムを展開しています。
+                <br>
+                <br>
+                <br>
+                *ブランドのすべてはフィクションでございます。*
+            </p>
+        </div>
         <div class="messageArea">
             <p class="messageLine">
-            -DELIVERY
-            <br>
-            ご購入いただいた商品は、ご入金確認後、3~5営業日以内に発送いたします。（土日祝除く）
-            <br>
-            <br>
-            配送業者の繁忙期・年末年始・悪天候時は、配送状況・交通事情によりお届けが遅れる場合がございます。あらかじめご了承ください。
-            <br>
-            <br>
-            配送料は一律880円いただいております。
-            <br>
-            <br>
-            <br>
-            -RETURNS AND EXCHANGES
-            <br>
-            お客様都合による返品・交換・キャンセルはお断り申し上げます。
-            <br>
-            <br>
-            商品の不良、ご配送がございましたらお問い合わせフォームよりご連絡をいただくようお願いいたします。
-            <br>
-            商品到着から7日以降の返品・交換はお受けできかねますので、商品ご到着後は速やかな商品状態確認のご対応よろしくお願いいたします。
-            <br>
-            <br>
-            <br>
-            -PAYMENT METHODS
-            <br>
-            以下のお支払方法をご利用いただけます。
-            <br>
-            ・クレジットカード決済
-            <br>
-            ・キャリア決済
-            <br>
-            ・銀行振込
-            <br>
-            ・コンビニ決済
-            <br>
-            ・PayPAL
+                -DELIVERY
+                <br>
+                ご購入いただいた商品は、ご入金確認後、3~5営業日以内に発送いたします。（土日祝除く）
+                <br>
+                <br>
+                配送業者の繁忙期・年末年始・悪天候時は、配送状況・交通事情によりお届けが遅れる場合がございます。
+                <br>
+                あらかじめご了承ください。
+                <br>
+                <br>
+                配送料は一律880円いただいております。
+                <br>
+                <br>
+                <br>
+                -RETURNS AND EXCHANGES
+                <br>
+                お客様都合による返品・交換・キャンセルはお断り申し上げます。
+                <br>
+                <br>
+                商品の不良、ご配送がございましたらお問い合わせフォームよりご連絡をいただくようお願いいたします。
+                <br>
+                <br>
+                商品到着から7日以降の返品・交換はお受けできかねますので、商品ご到着後は速やかな商品状態確認の
+                <br>
+                ご対応よろしくお願いいたします。
+                <br>
+                <br>
+                <br>
+                -PAYMENT METHODS
+                <br>
+                以下のお支払方法をご利用いただけます。
+                <br>
+                ・クレジットカード決済
+                <br>
+                ・キャリア決済
+                <br>
+                ・銀行振込
+                <br>
+                ・コンビニ決済
+                <br>
+                ・PayPAL
             </p>
         </div>
     </dev>
 
-    <footer>
+    <footer class="footer">
         <div class="lang">
             <select name="select_language">
                 <optgroup label="language">
@@ -133,8 +197,8 @@
             </select>
         </div>
 
-        <div class="contact_container">
-            <a href="">コンタクトフォーム</a>
+        <div>
+            <a class="contact_container" href="">コンタクトフォーム</a>
         </div>
     </footer>
 
