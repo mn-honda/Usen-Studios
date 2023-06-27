ff82771f333769a2df0ff33b9123fdc05388d99b
kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-27 17:09:11 +0900

    ヘッダー修正#79

--- resources/views/header_footer/footer.blade.php
+++ resources/views/header_footer/footer.blade.php
@@ -9,3 +9,5 @@
             <a class="contact_container" href="/inquiry">コンタクトフォーム</a>
         </div>
 </footer>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+<script src="js/index.js"></script>
