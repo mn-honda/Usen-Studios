9b8c826cac02131570407347652a114b2d2a61ec
USENGROUP\mn-honda <mn-honda@usen-next.jp>
2023-06-29 18:14:18 +0900

    クレジットカード画面編集

--- resources/views/user/contact/inquiry.blade.php
+++ resources/views/user/contact/inquiry.blade.php
@@ -13,26 +13,26 @@
         <p>{{$message}}</p>
     @endif
     
-        <h1>contact</h1>
+        <h1>CONTACT</h1>
         <p>お問い合わせ内容を入力してください</p>
 
         <div class="form_container">
         <form action="" method="post">
             <div class="a">
             返信先メールアドレス（必須）<br>
-            <input type="text" value="{{Auth::user()->email}}" required><br>
+            <input class="b" type="text" value="{{Auth::user()->email}}" required placeholder="Email"><br>
             </div>
             <div class="a">
             件名（必須）<br>
-            <input type="text" name="title" required><br>
+            <input class="b" type="text" name="title" required placeholder="Subject"><br>
             </div>
             <div class="a">
             注文詳細ID <br>
-            <input type="sale_detail_id" name="sale_detail_id"><br>
+            <input class="b" type="sale_detail_id" name="sale_detail_id" placeholder="OrderID"><br>
             </div>
             <div class="a">
             お問い合わせ詳細（必須）<br>
-            <textarea name="detail" cols="50" rows="20" required></textarea>
+            <textarea class="c" name="detail" cols="50" rows="20" required placeholder="Inquiry about the contact"></textarea>
             </div>
             <input type="hidden" name="user_id" value="{{Auth::user()->id}}"><br>
             <input type="submit" value="送信">
