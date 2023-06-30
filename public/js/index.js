d673813a2e7624fe6887ea5bb36c197861915b53
kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-30 13:07:49 +0900

    ヘッダーの軽微な修正

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,13 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
     $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
+
+$(".openbtn2").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+  $(".openbtn2").removeClass('active');//ボタンの activeクラスを除去し
+  $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
