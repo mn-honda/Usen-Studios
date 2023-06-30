e9e4066a4a47aa5f7bf74bc9b44a8fed9d64cf87
kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-30 11:10:33 +0900

    model修正

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,7 +22,7 @@ class Product extends Model
     }
 
     public function stock(){
-        return $this->hasOne(Purchase::class);
+        return $this->hasOne(Stock::class);
     }
     
     public function sizes(){
